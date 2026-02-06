38cca0f4a9033cf39798c6fed881d5485e789a6b
drleecheng <88688659+drleecheng@users.noreply.github.com>
2026-02-06 21:39:57 +0000

    47th

--- js/sketch.js
+++ js/sketch.js
@@ -138,6 +138,7 @@ function draw() {
     
     if ((frameCount%10 == 0)&&(gestures_results == 1))
     {
+      text("hello");
 		  //synth.set({"envelope": {"sustain": (640-pos.x)/640}});
       synth.volume.value = 100;
       let pitch = -pos.y/3+300;
