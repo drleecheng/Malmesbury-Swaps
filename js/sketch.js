7c81bf9d15179e974e466ff184c9901da72d701c
drleecheng <88688659+drleecheng@users.noreply.github.com>
2026-03-14 21:35:54 +0000

    189th

--- js/sketch.js
+++ js/sketch.js
@@ -67,7 +67,7 @@ function startWebcam() {
   if (window.setCameraStreamToMediaPipe) {
     cam = createCapture(VIDEO);
     cam.hide();
-    cam.elt.onloademetadata = function () {
+    cam.elt.onloadedmetadata = function () {
       if (cam.elt.videoWidth > 0 && cam.elt.videoHeight > 0) {
         window.setCameraStreamToMediaPipe(cam.elt);
       }
