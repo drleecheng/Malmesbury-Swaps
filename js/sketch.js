77a9e1368a4da38858860431aa7314ffca0f149d
drleecheng <88688659+drleecheng@users.noreply.github.com>
2026-03-14 21:38:17 +0000

    190th

--- js/sketch.js
+++ js/sketch.js
@@ -67,11 +67,12 @@ function startWebcam() {
   if (window.setCameraStreamToMediaPipe) {
     cam = createCapture(VIDEO);
     cam.hide();
-    cam.elt.onloadedmetadata = function () {
+    cam.elt.onplaying = () => {
       if (cam.elt.videoWidth > 0 && cam.elt.videoHeight > 0) {
         window.setCameraStreamToMediaPipe(cam.elt);
+        console.log("Camera connected to MediaPipe");
       }
-    }
+    };
   }
 }
 
